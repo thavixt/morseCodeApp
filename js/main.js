4f65b569abdadb88d8a96da1106cde11e9b77178
Peter Komlosi <komlosi.peti@gmail.com>
2017-05-17 11:39:14 +0200

    updated playback
    - playback now uses a chain of promises
    - auto-translate
    - updated styles
    - added quick mute option
    
    todo: options, speed, capitalize, etc

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
+// CONST & VARS
 // Textareas
-var asciiDiv = document.getElementById('ascii');
-var morseDiv = document.getElementById('morse');
-
+const asciiDiv = document.getElementById('ascii');
+const morseDiv = document.getElementById('morse');
 // Buttons
-var toMorseBtn = document.getElementById('toMorse');
-var toAsciiBtn = document.getElementById('toAscii');
-var playBtn = document.getElementById('play');
-
-// Status indicators
-var toMorseStatus = document.getElementById('toMorseStatus');
-var toAsciiStatus = document.getElementById('toAsciiStatus');
-var volumeIcon = document.getElementById('volumeIcon');
-
-
-var playStatus = document.getElementById('playStatus');
-var prevC = document.getElementById('prevChar');
-var curC = document.getElementById('currentChar');
-var nextC = document.getElementById('nextChar');
-
+/*var toMorseBtn = document.getElementById('toMorse');
+var toAsciiBtn = document.getElementById('toAscii');*/
+const playBtn = document.getElementById('play');
+// Status text
+const toMorseStatus = document.getElementById('toMorseStatus');
+const toAsciiStatus = document.getElementById('toAsciiStatus');
+// Player elements
+const playStatus = document.getElementById('playStatus');
+const prevC = document.getElementById('prevChar');
+const curC = document.getElementById('currentChar');
+const nextC = document.getElementById('nextChar');
 // Audio
-var audio = new Audio('audio/beep.wav');
-var volumeControl = document.getElementById('volumeControl');
+const audio = new Audio('audio/beep.wav');
+const volumeControl = document.getElementById('volumeControl');
+const volumeIcon = document.getElementById('volumeIcon');
 
 // Array of valid chars
-var valid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?', '!', '-', '/', '@', '(', ')', ' '];
+const valid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?', '!', '-', '/', '@', '(', ')', ' '];
 // Morse code chars json
 var morse;
 fetch('json/morse.json')
@@ -32,13 +29,13 @@ fetch('json/morse.json')
   .catch(error => console.log(error.message));
 
 
-// Functions
+// FUNCTIONS
+
 // Reset beep
 HTMLAudioElement.prototype.stop = function() {
   this.pause();
   this.currentTime = 0.0;
 }
-
 // Check if all characters of a string are convertable to Morse
 function checkChars(string) {
   let invalidChars = []; // array of invalid chars
@@ -60,7 +57,6 @@ function checkChars(string) {
     return "Invalid character(s):<br>" + invalidChars.join(" ");
   }
 };
-
 // Morse -> ASCII
 function decode(morseCode) {
   if(morseCode === '') {
@@ -77,7 +73,6 @@ function decode(morseCode) {
     ).join(' ') // words
   }
 };
-
 // ASCII -> MORSE
 function encode(string) {
   if(string === '') {
@@ -93,60 +88,12 @@ function encode(string) {
     ).join('   ') // words
   }
 };
-
 // Capitalize the first letter of each sentence
 function capitalize(string) {
   return string.split(/[.][ ]*/g).map(w => w[0].toUpperCase() + w.substr(1)).join('. ');
 };
-
-// Beep Morse code
-function play(morseCode) {
-  if(morseCode === '') {
-    return false;
-  }
-  var time = 200;
-  for(var i = 0; i < morseCode.length; i++) {
-    switch (morseCode[i]) {
-      case '.':
-        (function(i) {
-          time += 350;
-          setTimeout(function() {
-            audio.play();
-            currentPlaying(morseCode, i);
-          }, time+100);
-          setTimeout(function() { audio.stop(); }, (time) + 200);
-        })(i);
-        break;
-        
-      case '-':
-        (function(i) {
-          time += 550;
-          setTimeout(function() {
-            audio.play();
-            currentPlaying(morseCode, i);
-          }, time);
-          setTimeout(function() { audio.stop(); }, (time) + 400);
-        })(i);
-        break;
-        
-      case ' ':
-        (function(i) {
-          time += 800;
-          setTimeout(function() {
-            currentPlaying(morseCode, i);
-          }, time);
-        })(i);
-        break;
-        
-      default:
-        break;
-    }
-  }
-  return true;
-};
+// Update player status
 function currentPlaying(morseCode, i) {
-  morseCode.replace(/ /g, 'asd');
-  console.log(morseCode[i]);
   switch (i) {
     case 0:
       prevC.innerHTML = "&nbsp;";
@@ -176,16 +123,95 @@ function currentPlaying(morseCode, i) {
   }
 }
 
+// PLAYBACK
+function morsePlayback(str) {
+  // toggle elements
+  document.getElementById('playStatus').style.display = 'block';
+  document.getElementById('play').style.display = 'none';
 
-// Event listeners
-toMorseBtn.addEventListener('click', function() {
+  setTimeout(function() {
+    // start promise chain
+    var sequence = Promise.resolve();
+    // split string
+    str.split('').forEach(function(char, i) {
+      sequence = sequence.then(function() {
+        // process the current character
+        return playChar(str, char, i);
+      }).catch(function(err) {
+        // catch any error that happened along the way
+        console.log("Oops: " + err);
+      })
+    })
+  }, 300)
+  console.log("starting playback...");
+}
+
+function checkEnd(str, i) {
+  // if it reached the last character
+  if (str.length == i+1) {
+    setTimeout(function() {
+      document.getElementById('playStatus').style.display = 'none';
+      document.getElementById('play').style.display = 'block';
+      console.log('...playback ended.')
+    }, 700)
+  }
+}
+function playChar(str, char, i) {
+  return new Promise(function (resolve, reject) {
+    // character separator - pause - dont play beep
+    if(char === ' ') {
+      // update player status
+      currentPlaying(str, i);
+      setTimeout(function() {
+        // check if its the last char
+        resolve(checkEnd(str, i));
+      }, 400)
+    }
+    // morse character
+    else {
+      // short beep
+      if(char == '.') {
+        dur = 100;
+      } else {
+        // long beep
+        if(char == '-') {
+          dur = 300;
+        } else reject('Invalid character');
+      }
+      setTimeout(function () {
+        // update player status
+        currentPlaying(str, i);
+        // start beep
+        audio.play();
+        setTimeout(function () {
+          // stop beep
+          audio.stop();
+          // check if its the last char
+          resolve(checkEnd(str, i));
+        }, dur)
+      }, 100)
+    }
+  })
+};
+
+
+// EVENT LISTENERS
+// input
+asciiDiv.addEventListener('input', function() {
+  morseDiv.value = encode(asciiDiv.value);
+});
+morseDiv.addEventListener('input', function() {
+  asciiDiv.value = decode(morseDiv.value);
+});
+/*toMorseBtn.addEventListener('click', function() {
   morseDiv.value = encode(asciiDiv.value);
 });
 toAsciiBtn.addEventListener('click', function() {
   asciiDiv.value = decode(morseDiv.value);
-});
+});*/
+// player
 playBtn.addEventListener('click', function() {
-  play(morseDiv.value);
+  morsePlayback(morseDiv.value);
 });
 volumeControl.addEventListener('input', function() {
   audio.volume = volumeControl.value / 10;
@@ -199,9 +225,25 @@ volumeControl.addEventListener('input', function() {
     volumeIcon.innerHTML = "<i class='fa fa-volume-up'></i>";
   }
 });
+volumeIcon.addEventListener('click', function() {
+  if(audio.volume == 0) {
+    audio.volume = 0.4;
+    volumeControl.value = 4;
+    volumeIcon.innerHTML = "<i class='fa fa-volume-down'></i>";
+  }
+  else {
+    audio.volume = 0;
+    volumeControl.value = 0;
+    volumeIcon.innerHTML = "<i class='fa fa-volume-off'></i>";
+  }
+});
 
 // Page load
 (function() {
   morseDiv.value = "... --- ...";
+  asciiDiv.value = "sos";
   audio.volume = volumeControl.value / 10;
 })();
+
+
+
